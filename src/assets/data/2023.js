eb1096e38ee2615cb2e4d0e7cf3ec21ed1edc7fa
Sarin Udompanish <sarin.udompanish2@lseg.com>
2023-01-03 10:46:44 +0700

    chore: fix year

--- src/assets/data/2023.js
+++ src/assets/data/2023.js
@@ -1,67 +1,67 @@
 const holidays = {
   th: [
     {
-      date: '2022-01-02',
+      date: '2023-01-02',
       name: 'Substitution for New Year`s Day (Saturday 1st January 2022)',
     },
     {
-      date: '2022-03-06',
+      date: '2023-03-06',
       name: 'Makha Bucha Day',
     },
     {
-      date: '2022-04-06',
+      date: '2023-04-06',
       name: 'Chakri Memorial Day',
     },
     {
-      date: '2022-04-13',
+      date: '2023-04-13',
       name: 'Songkran Festival',
     },
     {
-      date: '2022-04-14',
+      date: '2023-04-14',
       name: 'Songkran Festival',
     },
     {
-      date: '2022-05-01',
+      date: '2023-05-01',
       name: 'National Labour Day',
     },
     {
-      date: '2022-05-04',
+      date: '2023-05-04',
       name: 'Coronation Day',
     },
     {
-      date: '2022-06-05',
+      date: '2023-06-05',
       name: 'Substitution for H.M. Queen Suthida Bajrasudhabimalalakshana Birthday',
     },
     {
-      date: '2022-07-28',
+      date: '2023-07-28',
       name: 'H.M. King Maha Vajiralongkorn Phra Vajiraklaochaoyuhua`s Birthday',
     },
     {
-      date: '2022-08-01',
+      date: '2023-08-01',
       name: 'Asarnha Bucha Day',
     },
     {
-      date: '2022-08-14',
+      date: '2023-08-14',
       name: 'Substitution for H.M. Queen Sirikit The Queen Mother`s Birthday / Mother`s Day',
     },
     {
-      date: '2022-10-13',
+      date: '2023-10-13',
       name: 'H.M. King Bhumibol Adulyadej The Great Memorial Day',
     },
     {
-      date: '2022-10-23',
+      date: '2023-10-23',
       name: 'Chulalongkorn Day',
     },
     {
-      date: '2022-12-05',
+      date: '2023-12-05',
       name: 'H.M. King Bhumibol Adulyadej The Great`s Birthday/ National Day / Father`s Day',
     },
     {
-      date: '2022-12-11',
+      date: '2023-12-11',
       name: 'Substitution for Constitution Day',
     },
     {
-      date: '2022-12-25',
+      date: '2023-12-25',
       name: 'Christmas Day',
     },
   ],
